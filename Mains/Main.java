452b0f13b2b3b2965a3db45211d321869c2473de
jangeissler22 <133253471+jangeissler22@users.noreply.github.com>
2023-05-12 07:08:20 +0200

    Update Main.java

--- Mains/Main.java
+++ Mains/Main.java
@@ -8,9 +8,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    //Test Anton
-    //testtt
-    //tet
     private static final ArrayList<Akteur> akteure = new ArrayList<>(); //ArrayList aller vorhandenen Akteure
 
     /**
